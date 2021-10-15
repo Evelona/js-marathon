a2c92bae4021164836e2ddc83bdf79355e1cf5c2
Aboba <evelina@mircod.com>
2021-10-16 00:52:15 +0300

    done homework-3

--- main.js
+++ main.js
@@ -1,49 +1,76 @@
+const createNode = (tag, classname) => {
+    const $tag = document.createElement(tag)
+    if (classname) $tag.classList.add(classname)
+
+    return $tag
+}
+
+const $randBtn = createNode('button', 'button')
 // Task #0
 const firstPlayer = {
+    player: 1,
     name: 'Sonya',
     hp: 50,
     img: 'http://reactmarathon-api.herokuapp.com/assets/sonya.gif',
     weapon: ['gun', 'rope'],
     attack: function() {
         console.log(this.name + ' - Fight..')
+    },
+    changeHP: function(hp) {
+        if (this.hp > 0) {
+            console.log(`${this.name} minus ${hp} hp`)
+            let reducedHP = this.hp - hp
+            this.hp = reducedHP >= 0? reducedHP : 0
+            if (this.hp === 0) {
+                console.log(`${secondPlayer.name} wins`)
+                $randBtn.disabled = true
+            }
+        }
     }
 }
 
 const secondPlayer = {
+    player: 2,
     name: 'Kitana',
     hp: 100,
     img: 'http://reactmarathon-api.herokuapp.com/assets/kitana.gif',
     weapon: ['pin'],
     attack: function() {
         console.log(this.name + ' - Fight..')
+    },
+    changeHP: function(hp) {
+        if (this.hp > 0) {
+            console.log(`${this.name} minus ${hp} hp`)
+            let reducedHP = this.hp - hp
+            this.hp = reducedHP >= 0? reducedHP : 0
+            if (this.hp === 0) {
+                console.log(`${firstPlayer.name} wins`)
+                $randBtn.disabled = true
+            }
+        }
     }
 }
 
-firstPlayer.attack()
+////////////////////////////////////////////////////////////////////
+
 
 // Task #1
 const $arenas = document.querySelector('div.arenas')
-const createPlayer = (playerClass, player) => {
-    const $player = document.createElement('div')
-    $player.classList.add(playerClass)
-    
 
-    const $progressbar = document.createElement('div')
-    $progressbar.classList.add('progressbar')
-    const $character = document.createElement('div')
-    $character.classList.add('character')
+const createPlayer = ( player) => {
+    const $player = createNode('div', 'player' + player.player)
+    const $progressbar = createNode('div', 'progressbar')
+    const $character = createNode('div', 'character')
 
-    const $life = document.createElement('div')
-    $life.classList.add('life')
+    const $life = createNode('div', 'life')
     $life.style.width=`${player.hp}%`
-    const $name = document.createElement('div')
-    $name .classList.add('name')
+    const $name = createNode('div','name')
     $name.innerHTML = player.name
 
     $progressbar.appendChild($life)
     $progressbar.appendChild($name)
 
-    const $img = document.createElement('img')
+    const $img = createNode('img')
     $img.src = player.img
     $character.appendChild($img)
 
@@ -51,15 +78,23 @@ const createPlayer = (playerClass, player) => {
     $player.appendChild($character)
 
 
-    $arenas.appendChild($player)
+    return $player
 }
 
+$arenas.appendChild(createPlayer( firstPlayer))
+$arenas.appendChild(createPlayer( secondPlayer))
 
-// Task #2
-// createPlayer('player1', 'SCORPION', 50);
-// createPlayer('player2', 'SUB-ZERO', 80);
+////////////////////////////////////////////////////////////////////
 
+const randomValue = Math.round(1 + Math.random() * 20)
 
-// Task #3
-createPlayer('player1', firstPlayer);
-createPlayer('player2', secondPlayer);
+const $control = createNode('div', 'control')
+// const $randBtn = createNode('button', 'button')
+$randBtn.innerHTML = 'Random'
+const players = [firstPlayer, secondPlayer]
+$randBtn.onclick = () => {
+    players[Math.round(Math.random()*1)].changeHP(Math.round(1 + Math.random() * 20))
+}
+
+$control.appendChild($randBtn)
+$arenas.appendChild($control)
